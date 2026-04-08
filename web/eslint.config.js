ffb4cea5a52c15c43a48446c19a849360f2f1630
Shawn Tabrizi <shawntabrizi@gmail.com>
2026-04-08 01:13:10 -0400

    update to match master

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,32 +1,14 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
-  {
-    ignores: ["dist", "node_modules", "tsconfig.tsbuildinfo"],
-  },
-  {
-    files: ["src/**/*.{ts,tsx}"],
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-    },
-  },
-  eslintConfigPrettier,
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	reactHooks.configs.flat["recommended-latest"],
+	eslintConfigPrettier,
+	{
+		ignores: ["dist/", "node_modules/", ".papi/"],
+	},
 );
